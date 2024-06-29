33d422c28a17e2413df400051f23ab4000416efd
MStroievus <108293369+MStroievus@users.noreply.github.com>
2024-06-29 21:41:26 +0300

    without hope

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,8 +8,8 @@ export default defineConfig({
   testDir: './tests/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 1,
-  workers: process.env.CI ? 1 : 2,
+  retries: process.env.CI ? 2 : 2,
+  workers: process.env.CI ? 2 : 2,
   reporter: 'html',
   use: {
     trace: 'retain-on-failure',
